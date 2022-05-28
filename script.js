f2d4b9effd9b5f9facd83c86c07ca6a3fc1d1863
sarband <81394460+SarbandAkray@users.noreply.github.com>
2022-05-28 22:45:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    navigator.geolocation.getCurrentPosition((possition)=>{
+        fetch('https://api.bigdatacloud.net/data/reverse-geocode-client?latitude='+possition.coords.latitude+'&longitude='+possition.coords.longitude+'&localityLanguage=ku')
+        .then(response => response.json())
+        .then(data =>{
+            console.log(data);
+            document.getElementById("continet").innerHTML=data.continent;
+            document.getElementById("country").innerHTML=data.countryName;
+            var city = document.getElementById("city");
+            if (data.localityInfo.administrative[2].name =="حەولێر"){
+                city.innerHTML="هەولێر";
+            }
+            
+            var map = L.map('map').setView([possition.coords.latitude,possition.coords.longitude], 18);
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19
+            }).addTo(map);
+            var marker = L.marker([possition.coords.latitude,possition.coords.longitude]).addTo(map);
+            
+        });
+    },
+    (err)=>{
+         document.getElementById("card").innerHTML ="<h1 style='color : red;'>error: </h1> <h1 style='color : red;'>please enable location in your browser</h1>";
+    }
+    );
+    })
